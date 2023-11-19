90dee4d928b06eb56d908889a1c16a7ccbb3a053
carollpq <107604758+carollpq@users.noreply.github.com>
2023-11-19 15:31:02 +0000

    Modify rotateImage() into javafx

--- src/main/java/example/GameUtil.java
+++ src/main/java/example/GameUtil.java
@@ -1,11 +1,7 @@
 package example;
 
-import java.awt.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.transform.Rotate;
-import javafx.scene.image.PixelReader;
-import javafx.scene.image.PixelFormat;
 
 public class GameUtil
 {
@@ -24,22 +20,11 @@ public class GameUtil
 		}
 	}
 
-	public static Image rotateImage(final BufferedImage bufferedImage, final int degree)
+	public static ImageView rotateImage(final Image image, final int degree)
 	{
-	int w = bufferedImage.getWidth();
-	int h = bufferedImage.getHeight();
-	int t = bufferedImage.getColorModel().getTransparency();
+		ImageView imageView = new ImageView(image);
+		imageView.setRotate(degree);
 
-	BufferedImage i;
-	Graphics2D graphics2d;
-
-	(graphics2d = (i = new BufferedImage(w, h, t)).createGraphics()).setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-
-	graphics2d.rotate(Math.toRadians(degree), w / 2, h / 2);
-	graphics2d.drawImage(bufferedImage, 0, 0, null);
-	graphics2d.dispose();
-
-	return i;
-
+		return imageView;
 	}
 }
