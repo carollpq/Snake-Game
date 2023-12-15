e67d3f2036cb5f4684d1b570920aeb19bb29fa31
carollpq <107604758+carollpq@users.noreply.github.com>
2023-12-15 20:03:34 +0000

    Add comments explaining the methods in EndingFrameController

--- src/main/java/example/Controller/EndingFrameController.java
+++ src/main/java/example/Controller/EndingFrameController.java
@@ -13,6 +13,7 @@ import java.util.Objects;
 
 public class EndingFrameController extends StartFrameController {
     @FXML
+    //Images in the 'Play Again' and 'Main Menu' buttons
     private ImageView playAgainBtnImg, mainMenuBtnImg;
 
     @FXML
@@ -23,27 +24,31 @@ public class EndingFrameController extends StartFrameController {
     }
 
     @FXML
+    //Set the image of 'Play Again' button when it is not hovered
     public void onPlayAgain() {
         playAgainBtnImg.setImage(ImageUtil.images.get("play-again-btn"));
     }
 
     @FXML
+    //Set the image of 'Play Again' button when it is hovered
     public void onPlayAgainHover() {
         playAgainBtnImg.setImage(ImageUtil.images.get("play-again-hover"));
     }
 
     @FXML
+    //Set the image of 'Main Menu' button when it is not hovered
     public void onMainMenu() {
         mainMenuBtnImg.setImage(ImageUtil.images.get("main-menu-btn"));
     }
 
     @FXML
+    //Set the image of 'Main Menu' button when it is hovered
     public void onMainMenuHover() {
         mainMenuBtnImg.setImage(ImageUtil.images.get("main-menu-hover"));
     }
     @FXML
     public void playAgain() throws IOException, InterruptedException {
-        scoreLabel.setVisible(false);
+        scoreLabel.setVisible(false); //Removes the label displaying high score
         if (Objects.equals(StartFrameMain.getCurrentMode(), "easy")) {
             playEasyMode();
         } else if (Objects.equals(StartFrameMain.getCurrentMode(), "medium")) {
@@ -55,12 +60,14 @@ public class EndingFrameController extends StartFrameController {
 
     @FXML
     public void initialize() {
+        //Sets the text for label displaying final score when FXML file is loaded
         setScoreLabel(MyFrameController.getFinalScore());
     }
 
     @Override
+    //Loads the Home page/Main Menu page after 'Main Menu' is clicked
     public void backToMainMenu() throws IOException {
-        scoreLabel.setVisible(false);
+        scoreLabel.setVisible(false); //Removes the label displaying high score
         super.backToMainMenu();
         StartFrameMain.changeMusic(new MusicPlayer("src/main/resources/cw1setup/Sounds/main-menu-music.mp3"));
     }
