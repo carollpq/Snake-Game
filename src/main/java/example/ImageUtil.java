23fcc74fde71210bbc5734a163a3259285edd5dc
carollpq <107604758+carollpq@users.noreply.github.com>
2023-12-15 21:58:22 +0000

    Delete classes that are not used

--- src/main/java/example/ImageUtil.java
+++ src/main/java/example/ImageUtil.java
@@ -39,7 +39,6 @@ public class ImageUtil
 		images.put("19", GameUtil.getImage("/cw1setup/Img/garnet.png"));
 		images.put("20", GameUtil.getImage("/cw1setup/Img/sapphire.png"));
 		//Background images
-		images.put("UI-background", GameUtil.getImage("/cw1setup/Img/UI-background.png"));
 		images.put("main-menu-scene", GameUtil.getImage("/cw1setup/Img/1629702755362_84a39ede3284dfcaa71703c98647a8f5.jpg"));
 		//Button images
 		images.put("resume-btn", GameUtil.getImage("/cw1setup/Img/resume-btn.png"));
