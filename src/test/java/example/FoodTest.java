518ae4638d8356de7d8f59e7550b5f18300a62bf
carollpq <107604758+carollpq@users.noreply.github.com>
2023-12-17 00:23:56 +0000

    Write tests for Food class

---
+++ src/test/java/example/FoodTest.java
@@ -0,0 +1,73 @@
+package example;
+
+import example.Model.ImageUtil;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FoodTest {
+    // Initialize JavaFX toolkit
+    JFXPanel jfxPanel = new JFXPanel();
+
+    @Test
+    void foodEatenIncreasesSnakeLengthAndScore() {
+        Platform.runLater(() -> {
+            ImageUtil.initializeImages();
+            int initialY = 100;
+            int initialX = 100;
+            MySnake mySnake = new MySnake(initialX, initialY, 5);
+            Food food = new Food();
+
+            // Manually set the position of the snake and food for testing
+            food.xPosition = 100;
+            food.yPosition = 100;
+
+            int initialLength = mySnake.getLength();
+            int initialScore = mySnake.score;
+
+            // Simulate food being eaten
+            food.eaten(mySnake);
+
+            // Check if the food is marked as not live
+            assertFalse(food.liveOfObject);
+
+            // Check if the snake length and score are increased
+            assertEquals(initialLength + 1, mySnake.getLength());
+            assertEquals(initialScore + 1, mySnake.score);
+
+        });
+    }
+
+    @Test
+    void foodNotEatenWhenSnakeNotIntersecting() {
+        Platform.runLater(() -> {
+            ImageUtil.initializeImages();
+            int initialY = 100;
+            int initialX = 100;
+            MySnake mySnake = new MySnake(initialX, initialY, 5);
+            Food food = new Food();
+
+            // Manually set the position of the snake and food for testing
+            mySnake.xPosition = 200;
+            mySnake.yPosition = 200;
+            food.xPosition = 300;
+            food.yPosition = 300;
+
+            int initialLength = mySnake.getLength();
+            int initialScore = mySnake.score;
+
+            // Simulate food not being eaten
+            food.eaten(mySnake);
+
+            // Check if the food is still live
+            assertTrue(food.liveOfObject);
+
+            // Check if the snake length and score are not changed
+            assertEquals(initialLength, mySnake.getLength());
+            assertEquals(initialScore, mySnake.score);
+
+        });
+    }
+}
